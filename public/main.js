f2ea9b5a5edc478d8f9ea316aa6106d771f10460
NicDef <nicolas.defee@gmx.de>
2024-07-21 22:03:16 +0200

    prototype

---
+++ public/main.js
@@ -0,0 +1,157 @@
+const socket = io();
+
+const footer = document.getElementById('footer');
+
+const start = () => {
+	const radios = document.querySelectorAll("input[type='radio']");
+	let cardValue;
+
+	// Loop through the radio buttons to find the checked one
+	for (const radio of radios) {
+		if (radio.checked) {
+			cardValue = radio.value;
+			break;
+		}
+	}
+
+	const handValue = document.getElementById('chooseHand').value;
+
+	socket.emit('start-game', { deck: parseInt(cardValue), hand: parseInt(handValue) });
+};
+
+let currentZIndex = 1;
+
+document.addEventListener('contextmenu', (event) => event.preventDefault());
+
+socket.on('hand', (data) => {
+	// Clear hand
+	Array.from(document.body.children).forEach((child) => {
+		if (child.classList.contains('card')) {
+			document.body.removeChild(child);
+		}
+	});
+	footer.innerHTML = '';
+
+	console.log(data.hand);
+
+	let offset = 50;
+	data.hand.forEach((card, i) => {
+		createCardInHand(card, offset * i);
+	});
+
+	// Create draw pile
+	const drawPile = document.createElement('div');
+	drawPile.classList.add('card');
+	drawPile.classList.add('draw-pile');
+	drawPile.id = 'drawPile';
+	drawPile.innerHTML = `Karte ziehen (${data.remaining})`;
+
+	drawPile.addEventListener('click', () => {
+		socket.emit('drawCard');
+	});
+
+	document.body.appendChild(drawPile);
+});
+
+const createCardInHand = (card) => {
+	const div = document.createElement('div');
+	div.style.backgroundImage = `url(./svg/${card}.svg`;
+	div.classList.add('card');
+	div.id = card; // TODO: id's must be unique
+	footer.appendChild(div);
+
+	div.addEventListener('mouseup', (e) => {
+		if (e.button == 0 && !Array.from(document.body.children).includes(div)) {
+			div.style.position = 'absolute';
+			div.style.top = '0px';
+
+			document.body.appendChild(div);
+
+			const position = { x: div.style.left, y: div.style.top };
+			socket.emit('createCard', { id: div.id, position: position });
+
+			addDnD(div);
+		} else if (e.button == 2 && Array.from(document.body.children).includes(div)) {
+			div.style.position = 'relative';
+			div.style.top = '0px';
+			div.style.left = '0px';
+
+			footer.appendChild(div);
+
+			socket.emit('removeCard', { id: div.id });
+		}
+	});
+};
+
+const addDnD = (div) => {
+	let drag = false;
+	let shiftX, shiftY;
+	div.addEventListener('mousedown', (e) => {
+		if (e.button == 0 && Array.from(document.body.children).includes(div)) {
+			drag = true;
+
+			div.style.zIndex = ++currentZIndex;
+
+			shiftX = e.clientX - div.getBoundingClientRect().left;
+			shiftY = e.clientY - div.getBoundingClientRect().top;
+
+			div.style.cursor = 'grabbing';
+		} else drag = false;
+	});
+
+	document.addEventListener('mousemove', (e) => {
+		if (!drag) return;
+
+		div.style.left = e.pageX - shiftX + 'px';
+		div.style.top = e.pageY - shiftY + 'px';
+
+		const newPosition = { x: div.style.left, y: div.style.top };
+
+		socket.emit('updatePosition', { id: div.id, position: newPosition });
+	});
+
+	div.addEventListener('mouseup', (e) => {
+		if (drag && e.button == 0) {
+			drag = false;
+			div.style.cursor = 'grab	';
+		}
+	});
+};
+
+socket.on('createCard', (data) => {
+	const div = document.createElement('div');
+	div.style.backgroundImage = `url(./svg/${data.id}.svg`;
+	div.classList.add('card');
+	div.style.position = 'absolute';
+	div.id = data.id; // TODO: id's must be unique
+
+	div.style.left = data.position.x;
+	div.style.top = data.position.y;
+
+	addDnD(div);
+
+	document.body.appendChild(div);
+});
+
+socket.on('updatePosition', (data) => {
+	const div = document.getElementById(data.id);
+
+	div.style.left = data.position.x;
+	div.style.top = data.position.y;
+});
+
+socket.on('removeCard', (data) => {
+	const div = document.getElementById(data.id);
+
+	div.style.position = 'relative';
+	div.style.left = 0;
+	div.style.top = 0;
+
+	footer.appendChild(div);
+});
+
+socket.on('drawCard', (data) => {
+	createCardInHand(data.card);
+
+	document.getElementById('drawPile').innerHTML = `Karte ziehen (${data.remaining})`;
+});
---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+const socketio = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const url = `http://localhost:${PORT}`;
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start server
+server.listen(PORT, () => console.log(`Server is running on Port ${PORT} --> ` + url));
+
+// const deck32 = [];
+const deck32 = ['AC', 'KC', 'QC', 'JC', 'TC', '9C', '8C', '7C', 'AD', 'KD', 'QD', 'JD', 'TD', '9D', '8D', '7D', 'AH', 'KH', 'QH', 'JH', 'TH', '9H', '8H', '7H', 'AS', 'KS', 'QS', 'JS', 'TS', '9S', '8S', '7S'];
+const deck52 = ['A', 'K', 'D', 'B', '10', '9', '8', '7', '6', '5', '4', '3', '2', 'A', 'K', 'D', 'B', '10', '9', '8', '7', '6', '5', '4', '3', '2', 'A', 'K', 'D', 'B', '10', '9', '8', '7', '6', '5', '4', '3', '2', 'A', 'K', 'D', 'B', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
+
+const connections = [];
+
+var drawPile = [];
+
+io.on('connection', (socket) => {
+	console.log(`Player has connected`);
+	connections.push(socket.id);
+	console.log(connections);
+
+	// Handle disconnect
+	socket.on('disconnect', () => {
+		console.log(`Player disconnected`);
+		connections.splice(connections.indexOf(socket.id), 1);
+	});
+
+	socket.on('start-game', (params) => {
+		var deck;
+		switch (params.deck) {
+			case 32:
+				deck = [...deck32];
+				break;
+			case 52:
+				deck = [...deck52];
+				break;
+			default:
+				return;
+		}
+
+		// Shuffle cards
+		const fisherYatesShuffle = (deck) => {
+			for (var i = deck.length - 1; i > 0; i--) {
+				const swapIndex = Math.floor(Math.random() * (i + 1));
+				const currentCard = deck[i];
+				const cardToSwap = deck[swapIndex];
+				deck[i] = cardToSwap;
+				deck[swapIndex] = currentCard;
+			}
+
+			return deck;
+		};
+		const shuffled = fisherYatesShuffle(deck);
+		console.log(shuffled);
+
+		// Give every player one hand
+		for (let i = 0; i <= connections.length - 1; i++) {
+			(function (i) {
+				let hand = [];
+				for (let j = 0; hand.length < params.hand; j += connections.length) {
+					hand.push(shuffled[i + j]);
+					shuffled[i + j] = null;
+				}
+				console.log(hand);
+				io.to(connections[i]).emit('hand', { hand: hand, remaining: params.deck - connections.length * params.hand });
+			})(i);
+		}
+
+		drawPile = shuffled.filter((item) => item !== null);
+	});
+
+	socket.on('updatePosition', (data) => {
+		socket.broadcast.emit('updatePosition', data);
+	});
+
+	socket.on('createCard', (data) => {
+		socket.broadcast.emit('createCard', data);
+	});
+
+	socket.on('removeCard', (data) => {
+		socket.broadcast.emit('removeCard', data);
+	});
+
+	socket.on('drawCard', () => {
+		if (drawPile.length == 0) return;
+		socket.emit('drawCard', { card: drawPile[0], remaining: drawPile.length - 1 });
+		drawPile.shift();
+	});
+});
